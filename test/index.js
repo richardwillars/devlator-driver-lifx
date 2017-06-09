a6d9c84aed18a47bf62a3615a8dbc0a718d6a6d7
Richard Willars <richard@richardwilllars.com>
2017-06-09 16:56:56 +0100

    Updated with new command system and event handlers

--- test/index.js
+++ test/index.js
@@ -1,287 +1,33 @@
-var chai = require('chai');
-var expect = chai.expect;
+/* eslint-disable new-cap, no-unused-expressions, no-undef, global-require */
+const chai = require('chai');
+const mockery = require('mockery');
+const driverTests = require('thinglator/utils/testDriver');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
 
-var driverName = 'lifx';
-var driverType = 'light';
-var driverInterface = 'http';
+const expect = chai.expect;
+chai.use(sinonChai);
 
-describe('driver structure', () => {
-	describe('driver methods', () => {
-		it('should be exposed as a class', () => {
-			var driver = require('../index');
-			expect(driver).to.be.a.class;
-		});
+const driverName = 'lifx';
+const driverType = 'light';
+const driverInterface = 'http';
 
-		it('should have a constructor that accepts an instance of the driver settings class and an object containing a list of interfaces', () => {
-			var driverSettings = new class DriverSettings {
-				get() {
-					return Promise.resolve({});
-				}
-				set(settings) {
-					return Promise.resolve();
-				}
-			}
 
-			var interfaces = {
-				http: {}
-			};
+describe('Lifx', () => {
+    const yaLifxMock = {
+        listLights: sinon.stub().returns(Promise.resolve()),
+        setState: sinon.stub().returns(Promise.resolve()),
+        breathe: sinon.stub().returns(Promise.resolve()),
+        init: sinon.stub()
+    };
 
-			var driver = require('../index');
-			var driverInstance = new driver(driverSettings, interfaces);
-			expect(driverInstance instanceof driver).to.equal(true);
-		});
+    mockery.enable({
+        useCleanCache: true,
+        warnOnUnregistered: false
+    });
 
-		it('should have a getName method that returns the name of the driver', () => {
-			var driverSettings = new class DriverSettings {
-				get() {
-					return Promise.resolve({});
-				}
-				set(settings) {
-					return Promise.resolve();
-				}
-			}
+    mockery.registerMock('ya-lifx', yaLifxMock);
+    const Driver = require('../index');
 
-			var interfaces = {
-				http: {}
-			};
-
-			var driver = require('../index');
-			var driverInstance = new driver(driverSettings, interfaces);
-			expect(driverInstance.getName).to.be.a.function;
-			expect(driverInstance.getName()).to.equal(driverName);
-		});
-
-		it('should have a getType method that returns the type of the driver', () => {
-			var driverSettings = new class DriverSettings {
-				get() {
-					return Promise.resolve({});
-				}
-				set(settings) {
-					return Promise.resolve();
-				}
-			}
-
-			var interfaces = {
-				http: {}
-			};
-
-			var driver = require('../index');
-			var driverInstance = new driver(driverSettings, interfaces);
-			expect(driverInstance.getType).to.be.a.function;
-			expect(driverInstance.getType()).to.equal(driverType);
-		});
-
-		it('should have a getInterface method that returns the interface required by the driver', () => {
-			var driverSettings = new class DriverSettings {
-				get() {
-					return Promise.resolve({});
-				}
-				set(settings) {
-					return Promise.resolve();
-				}
-			}
-
-			var interfaces = {
-				http: {}
-			};
-
-			var driver = require('../index');
-			var driverInstance = new driver(driverSettings, interfaces);
-			expect(driverInstance.getInterface).to.be.a.function;
-			expect(driverInstance.getInterface()).to.equal(driverInterface);
-		});
-
-		it('should have a setEventEmitter method that accepts an event emitter', () => {
-			var driverSettings = new class DriverSettings {
-				get() {
-					return Promise.resolve({});
-				}
-				set(settings) {
-					return Promise.resolve();
-				}
-			}
-
-			var interfaces = {
-				http: {}
-			};
-
-			var driver = require('../index');
-			var driverInstance = new driver(driverSettings, interfaces);
-			expect(driverInstance.setEventEmitter).to.be.a.function;
-		});
-
-		it('should have an initDevices method that accepts an array of devices to be initialised if required', () => {
-			var driverSettings = new class DriverSettings {
-				get() {
-					return Promise.resolve({});
-				}
-				set(settings) {
-					return Promise.resolve();
-				}
-			}
-
-			var interfaces = {
-				http: {}
-			};
-
-			var driver = require('../index');
-			var driverInstance = new driver(driverSettings, interfaces);
-			expect(driverInstance.initDevices).to.be.a.function;
-		});
-
-		it('should have a getAuthenticationProcess method that returns the authorisation process for the driver as an array', () => {
-			var driverSettings = new class DriverSettings {
-				get() {
-					return Promise.resolve({});
-				}
-				set(settings) {
-					return Promise.resolve();
-				}
-			}
-
-			var interfaces = {
-				http: {}
-			};
-
-			var driver = require('../index');
-			var driverInstance = new driver(driverSettings, interfaces);
-			expect(driverInstance.getAuthenticationProcess).to.be.a.function;
-			expect(driverInstance.getAuthenticationProcess()).to.be.an.array;
-		});
-
-		it('should have a discover method that promises to find and return all active devices', () => {
-			var driverSettings = new class DriverSettings {
-				get() {
-					return Promise.resolve({});
-				}
-				set(settings) {
-					return Promise.resolve();
-				}
-			}
-
-			var interfaces = {
-				http: {}
-			};
-
-			var driver = require('../index');
-			var driverInstance = new driver(driverSettings, interfaces);
-			expect(driverInstance.discover).to.be.a.function;
-		});
-	});
-
-	describe('capability methods', () => {
-		it('should have an \'capability_setHSBState\' method that promises to set the colour state of a lightbulb', () => {
-			var driverSettings = new class DriverSettings {
-				get() {
-					return Promise.resolve({});
-				}
-				set(settings) {
-					return Promise.resolve();
-				}
-			}
-
-			var interfaces = {
-				http: {}
-			};
-
-			var driver = require('../index');
-			var driverInstance = new driver(driverSettings, interfaces);
-			expect(driverInstance.capability_setHSBState).to.be.a.function;
-		});
-
-		it('should have an \'capability_setBrightnessState\' method that promises to set the brightness of a lightbulb', () => {
-			var driverSettings = new class DriverSettings {
-				get() {
-					return Promise.resolve({});
-				}
-				set(settings) {
-					return Promise.resolve();
-				}
-			}
-
-			var interfaces = {
-				http: {}
-			};
-
-			var driver = require('../index');
-			var driverInstance = new driver(driverSettings, interfaces);
-			expect(driverInstance.capability_setBrightnessState).to.be.a.function;
-		});
-
-		it('should have an \'capability_setBooleanState\' method that promises to turn a lightbulb on or off', () => {
-			var driverSettings = new class DriverSettings {
-				get() {
-					return Promise.resolve({});
-				}
-				set(settings) {
-					return Promise.resolve();
-				}
-			}
-
-			var interfaces = {
-				http: {}
-			};
-
-			var driver = require('../index');
-			var driverInstance = new driver(driverSettings, interfaces);
-			expect(driverInstance.capability_setBooleanState).to.be.a.function;
-		});
-
-		it('should have an \'capability_toggle\' method that promises to toggle a lightbulb', () => {
-			var driverSettings = new class DriverSettings {
-				get() {
-					return Promise.resolve({});
-				}
-				set(settings) {
-					return Promise.resolve();
-				}
-			}
-
-			var interfaces = {
-				http: {}
-			};
-
-			var driver = require('../index');
-			var driverInstance = new driver(driverSettings, interfaces);
-			expect(driverInstance.capability_off).to.be.a.function;
-		});
-
-		it('should have an \'capability_breatheEffect\' method that promises to apply a breatheEffect to a lightbulb', () => {
-			var driverSettings = new class DriverSettings {
-				get() {
-					return Promise.resolve({});
-				}
-				set(settings) {
-					return Promise.resolve();
-				}
-			}
-
-			var interfaces = {
-				http: {}
-			};
-
-			var driver = require('../index');
-			var driverInstance = new driver(driverSettings, interfaces);
-			expect(driverInstance.capability_breatheEffect).to.be.a.function;
-		});
-
-		it('should have an \'capability_pulseEffect\' method that promises to apply a pulseEffect to a lightbulb', () => {
-			var driverSettings = new class DriverSettings {
-				get() {
-					return Promise.resolve({});
-				}
-				set(settings) {
-					return Promise.resolve();
-				}
-			}
-
-			var interfaces = {
-				http: {}
-			};
-
-			var driver = require('../index');
-			var driverInstance = new driver(driverSettings, interfaces);
-			expect(driverInstance.capability_pulseEffect).to.be.a.function;
-		});
-	});
+    driverTests(driverName, Driver, driverType, driverInterface, expect);
 });
